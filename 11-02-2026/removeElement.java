fee198fe657c9ba34b6f50b5c287fa5dfc7a5cf8
Karthik Bhaskar Magadi <karthik.magadi@bounteous.com>
2026-02-11 09:45:58 -0800

    Solved LC 27

---
+++ 11-02-2026/removeElement.java
@@ -0,0 +1,18 @@
+class removeElement 
+{
+    public int removeEle(int[] nums, int val) 
+    {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}

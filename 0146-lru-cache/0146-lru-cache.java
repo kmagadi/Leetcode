af980e752af2fb0ca7145d67840d8b5c263c8dfe
kmagadi <karthik.magadi@bounteous.com>
2026-02-25 22:52:12 +0530

    Time: 1037 ms (5.02%), Space: 109.9 MB (100%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,46 @@
+class LRUCache {
+    int capacity;
+    int[][] arr;
+    int currSize;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.arr = new int[capacity][2];
+        this.currSize = 0;
+    }
+
+    public int get(int key) {
+        for (int i = 0; i < currSize; i++) {
+            if (arr[i][0] == key) {
+                int val = arr[i][1];
+                for (int j = i; j < currSize - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr[currSize - 1] = new int[]{key, val};
+                return val;
+            }
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        for (int i = 0; i < currSize; i++) {
+            if (arr[i][0] == key) {
+                for (int j = i; j < currSize - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr[currSize - 1] = new int[]{key, value};
+                return;
+            }
+        }
+
+        if (currSize < capacity) {
+            arr[currSize++] = new int[]{key, value};
+        } else {
+            for (int i = 0; i < capacity - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[capacity - 1] = new int[]{key, value};
+        }
+    }
+}

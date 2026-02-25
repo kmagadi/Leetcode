1030b67c3a6d650751a9ede1326b4cab352f9985
kmagadi <karthik.magadi@bounteous.com>
2026-02-25 22:52:47 +0530

    Time: 1101 ms (5.02%), Space: 131.5 MB (48.51%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -44,3 +44,10 @@ class LRUCache {
         }
     }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */

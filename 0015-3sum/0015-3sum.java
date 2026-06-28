1408a38910432d9f3b5613c956000a8fbb9da939
kmagadi <karthik.magadi@bounteous.com>
2026-06-28 19:33:46 +0530

    Time: 34 ms (60.93%), Space: 59.4 MB (24.9%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,40 +2,47 @@ class Solution
 {
     public List< List< Integer >> threeSum( int[] nums ) 
     {
-        List< List< Integer >> resList = new ArrayList<>();
         Arrays.sort( nums );
-
-        for ( int i = 0; i < nums.length - 2; i++ ) 
+        List< List< Integer >> resList = new ArrayList<>();
+        for( int i = 0; i < nums.length - 2; i++ )
         {
-            if ( i > 0 && nums[ i ] == nums[ i - 1 ] ) continue;
-
-            int start = i + 1;
-            int end = nums.length - 1;
-
-            while ( start < end ) 
+            if (i > 0 && nums[i] == nums[i - 1]) 
             {
-                int sum = nums[ i ] + nums[ start ] + nums[ end ];
-
-                if ( sum == 0 ) 
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
+            while( j < k )
+            {
+                int sum = nums[ i ] + nums[ j ] + nums[ k ];
+                if( sum == 0 )
                 {
-                    resList.add(Arrays.asList( nums[ i ], nums[ start ], nums[ end ]));
+                    resList.add( Arrays.asList( nums[ i ], nums[ j ], nums[ k ]));
                     
-                    while ( start < end && nums[ start ] == nums[ start + 1 ]) start++;
-                    while ( start < end && nums[ end ] == nums[ end - 1 ]) end--;
+                    while ( j < k && nums[ j ] == nums[ j + 1 ]) 
+                    {
+                        j++;
+                    }
                     
-                    start++;
-                    end--;
-                } 
-                else if ( sum < 0 ) 
+                    while ( j < k && nums[ k ] == nums[ k - 1 ]) 
+                    {
+                        k--;
+                    }
+                    
+                    j++;
+                    k--;
+                }
+                else if( sum > 0 )
                 {
-                    start++;
-                } 
-                else 
+                    k--;
+                }
+                else
                 {
-                    end--;
+                    j++;
                 }
             }
-        }
+        }       
+
         return resList;
     }
 }
